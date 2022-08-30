0af4b9b265d4150e72a74c60a176cd737616c198
Adejori Eniola <enx.co21@gmail.com>
2022-08-30 19:58:25 +0100

    Sign up page added

--- client/myapp/src/Components/Landing-Page/Create/Create.jsx
+++ client/myapp/src/Components/Landing-Page/Create/Create.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import create from "../../../Asset/illustration_Create.svg";
 
 const Create = () => {
-  return <div>Create</div>;
+  return (
+    <Flex display={"flex"} mx={"50px"} w={"100%"} h={"100%"}>
+      <Box>
+        <Heading noOfLines={2} mt={"50px"}>
+          Create task and collaborate with your team effectively.
+        </Heading>
+        <Text mt={"20px"} noOfLines={2} ml={"10px"}>
+          Protrack is a task management app designed to help individual and
+          teams work together in one unified workspace.
+        </Text>
+      </Box>
+      <Box m={"auto"} mt={"30px"} mr={"30px"}>
+        <Image src={create} />
+      </Box>
+    </Flex>
+  );
 };
 
 export default Create;
